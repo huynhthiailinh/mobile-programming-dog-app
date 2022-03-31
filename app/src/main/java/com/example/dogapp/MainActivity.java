3bd036c62338fe908e35ae652a700ae5cd62160a
HUYNHTHIAILINH <huynhthiailinh2105@gmail.com>
2022-03-31 22:18:11 +0700

    Refactoring the App Nav ListFragment

--- app/src/main/java/com/example/dogapp/MainActivity.java
+++ app/src/main/java/com/example/dogapp/MainActivity.java
@@ -1,31 +1,14 @@
 package com.example.dogapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import com.example.dogapp.databinding.ActivityMainBinding;
-import com.example.dogapp.model.DogBreed;
-import com.example.dogapp.viewmodel.DogsAdapter;
-import com.example.dogapp.viewmodel.DogsApiService;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.annotations.NonNull;
-import io.reactivex.rxjava3.observers.DisposableSingleObserver;
-import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-    private DogsApiService apiService;
     private ActivityMainBinding binding;
-    private DogsAdapter dogsAdapter;
-    private ArrayList<DogBreed> dogBreeds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,30 +17,5 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        dogBreeds = new ArrayList<DogBreed>();
-        dogsAdapter = new DogsAdapter(dogBreeds);
-
-        binding.rvDogs.setLayoutManager(new GridLayoutManager(this, 2));
-        binding.rvDogs.setAdapter(dogsAdapter);
-
-        apiService = new DogsApiService();
-        apiService.getDogs()
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(new DisposableSingleObserver<List<DogBreed>>() {
-                    @Override
-                    public void onSuccess(@NonNull List<DogBreed> dogBreedList) {
-                        for (DogBreed dog : dogBreedList) {
-                            Log.d("DEBUG1", dog.getName());
-                            dogBreeds.add(dog);
-                            dogsAdapter.notifyDataSetChanged();
-                        }
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-                        Log.d("DEBUG1", e.getMessage());
-                    }
-                });
     }
 }
---
+++ app/src/main/java/com/example/dogapp/view/ListFragment.java
@@ -0,0 +1,78 @@
+package com.example.dogapp.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dogapp.databinding.FragmentListBinding;
+import com.example.dogapp.model.DogBreed;
+import com.example.dogapp.viewmodel.DogsAdapter;
+import com.example.dogapp.viewmodel.DogsApiService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.observers.DisposableSingleObserver;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class ListFragment extends Fragment {
+    private DogsApiService apiService;
+    private FragmentListBinding binding;
+    private DogsAdapter dogsAdapter;
+    private ArrayList<DogBreed> dogBreeds;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+        binding = FragmentListBinding.inflate(getLayoutInflater());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = binding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@androidx.annotation.NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        dogBreeds = new ArrayList<DogBreed>();
+        dogsAdapter = new DogsAdapter(dogBreeds);
+
+        binding.rvDogs.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        binding.rvDogs.setAdapter(dogsAdapter);
+
+        apiService = new DogsApiService();
+        apiService.getDogs()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableSingleObserver<List<DogBreed>>() {
+                    @Override
+                    public void onSuccess(@NonNull List<DogBreed> dogBreedList) {
+                        for (DogBreed dog : dogBreedList) {
+                            Log.d("DEBUG1", dog.getName());
+                            dogBreeds.add(dog);
+                            dogsAdapter.notifyDataSetChanged();
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        Log.d("DEBUG1", e.getMessage());
+                    }
+                });
+    }
+}
